259229a1954d9b4e0bb57cb5b3339c98a9c7f311
Bailey Franklin <baileydfranklin@gmail.com>
2023-04-11 21:39:20 -0700

    finished assignment

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import MoleContainer from './components/MoleContainer';
+import { useState } from 'react';
+
+import './App.css';
+
+function App() {
+  let [score, setScore] = useState(0)
+
+  const createMoleHill = (numHills = 9) => {
+    let hills = [];
+    for( let i = 0; i < numHills; i++) {
+      hills.push(
+        <MoleContainer 
+          key={i}
+          setScore={setScore}
+          score={score}
+        
+        />
+      );
+    }
+    return (
+      <div className='mole-hills'>
+        {hills}
+      </div>
+    )
+  }
+
+  return (
+    <div className="App">
+      <h2>{score}</h2>
+      {createMoleHill()}
+
+    </div>
+  );
+}
+
+export default App;
